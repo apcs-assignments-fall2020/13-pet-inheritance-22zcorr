0221a774e3c987f43d7841b98c161f5cca28cb1e
22zcorr <22zcorr@athenian.org>
2020-10-29 21:26:36 +0000

    wrote the code

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,18 +1,40 @@
 public class Dog extends Pet {
     // Instance variable(s)
-
+    private String breed;
 
     // Constructors
-    
-
+    public Dog(String name, int age, String breed) {
+        this.setName(name);
+        this.setAge(age);
+        this.breed=breed;
+    }
+    public Dog() {
+        super();
+        this.breed="yellow";
+    }
     // makeNoise() method
+    public void makeNoise() {
+        System.out.println("* d o g  n o i s e *!");
+    }
 
 
     // toString method
+    public String toString(){
+        return "Name: "+this.getName()+"\nAge: "+this.getAge()+"\nBreed: "+this.breed;
+    }
 
 
     // Getter
-
+    public String getBreed(){
+        return this.breed;
+    }
 
     // Setter
+    public void setBreed(String a){
+        if(a.trim().length()>0){
+            this.breed=a;
+        }
+    }
+
+
 }
--- src/main/java/DogClient.java
+++ src/main/java/DogClient.java
@@ -1,5 +1,7 @@
 public class DogClient {
     public static void main(String[] args) {
-        
+        Dog bob = new Dog("bob",9,"black");
+        System.out.println(bob);
+        bob.makeNoise();
     }
 }
